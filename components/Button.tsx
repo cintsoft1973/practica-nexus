c7a062996f5e34c0fddf244835a53ab74ce3bc43
Rakso the king <etic.srl@gmail.com>
2026-03-26 00:48:41 -0400

    2B

---
+++ components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+type ButtonProps = {
+    children: React.ReactNode;
+    onClick: () => void;
+}
+
+export default function Button({ children, onClick }: ButtonProps) {
+    return (
+        <button onClick={onClick}
+            className='bg-blue-600 text-white px-4 py-2 rounded'
+        >
+            {children}
+        </button>
+    )
+}
